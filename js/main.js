1cdf29f77a6b5344f0b56081d804acbf8295bc5f
Thomas Reschenhofer <thomas.reschenhofer@tum.de>
2015-10-27 10:37:54 +0100

    bugfix

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function updateContent(post) {
 
 $(document).ready(function () {
     $(".navbar-nav>li>a").each(function () {
-        if (window.location.href == this.href || (window.location.href.indexOf("/applications/") >= 0 && this.href.indexOf("/applications/") >= 0)) {
+        if (window.location.href == this.href || (window.location.href.indexOf("/application/") >= 0 && this.href.indexOf("/applications/") >= 0)) {
             $(this).addClass("nav-active");
         }
     });
