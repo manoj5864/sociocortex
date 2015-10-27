2df8d8275999cb9c197f3789f7811b0e35dcfcad
Thomas Reschenhofer <thomas.reschenhofer@tum.de>
2015-10-27 10:03:19 +0100

    added style for current page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function updateContent(post) {
     alert(post);
 }
+
+$(document).ready(function () {
+    $(".navbar-nav>li>a").each(function () {
+        if (this.href == window.location.href) {
+            $(this).addClass("nav-active");
+        }
+    });
+});
